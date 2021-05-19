4dddf85304837a1e4f97b5695042e6bdca39a013
cbaerajas <83188673+cbaerajas@users.noreply.github.com>
2021-05-19 12:53:27 -0700

    jsdoc

--- demo.js
+++ demo.js
@@ -8,23 +8,35 @@ var btn = new MyToolkit.Button(draw)
 btn.move(50,1);
 btn.setText('hello');
 
-//btn.onclick(function(e){
-//	console.log(e);
-//});
-//btn.onmouseout(function(e){
-//    console.log(e);
-//});
-//btn.onmouseover(function(e){
-//    console.log(e);
-//});
-//btn.onmouseup(function(e){
-//    console.log(e);
-//})
+btn.onclick(function(e){
+	console.log(e);
+});
+btn.onmouseout(function(e){
+   console.log(e);
+});
+btn.onmouseover(function(e){
+   console.log(e);
+});
+btn.onmouseup(function(e){
+   console.log(e);
+})
 
 // Implement a MyToolkit Check Box
 var cbox = new MyToolkit.Checkbox(draw);
 cbox.move(50, 70);
 cbox.setText('check me out')
+cbox.onclick(function(e){
+    console.log(e);
+})
+cbox.onmouseout(function(e){
+    console.log(e);
+})
+cbox.onmouseover(function(e){
+    console.log(e);
+})
+cbox.onmouseup(function(e){
+    console.log(e);
+})
 
 //Implement a MyToolkit Radio Button
 var radio = new MyToolkit.RadioButton(draw);
@@ -36,18 +48,52 @@ radio3.move(50,320);
 radio.setText('tune me')
 radio2.setText('these')
 radio3.setText('dont work :(')
+radio.onclick(function(e){
+    console.log(e)
+})
+radio2.onclick(function(e){
+    console.log(e)
+})
+radio3.onclick(function(e){
+    console.log(e)
+})
 
+radio.onmouseout(function(e){
+    console.log(e)
+})
+radio2.onmouseout(function(e){
+    console.log(e)
+})
+radio3.onmouseout(function(e){
+    console.log(e)
+})
+
+radio.onmouseover(function(e){
+    console.log(e)
+})
+radio2.onmouseover(function(e){
+    console.log(e)
+})
+radio3.onmouseover(function(e){
+    console.log(e)
+})
 
 //Implement a MyToolkit Custom Widget
 var switcher = new MyToolkit.CustomWidget(draw);
 switcher.move(50,130);
 switcher.setText('switch me for dark mode!')
+switcher.onclick(function(e){
+    console.log(e);
+})
 
 //Implement a MyToolkit Progess Bar
 var progress = new MyToolkit.ProgressBar(draw);
 progress.setWidth(200);
 progress.move(50, 200);
 progress.setProgress(50)
+progress.onmouseover(function(e){
+    console.log(e)
+})
 
 //Implement a MyToolkit Scroll Bar
 var scroll = new MyToolkit.ScrollBar(draw);
